ffaf970d3343b7ad2cfd9fe95eb1e553e04118d0
abdalshafiealmajdoup <abdalshafiealmajdoup>
2024-04-14 14:59:20 +0200

    add sms center car

--- src/components/SideBarCenter.tsx
+++ src/components/SideBarCenter.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { CDBSidebar, CDBSidebarContent, CDBSidebarFooter, CDBSidebarHeader, CDBSidebarMenu, CDBSidebarMenuItem, } from 'cdbreact';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { logoutServiceCenter } from '../store/slices/ServiceCenter'; 
+import React, { useEffect,useState } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate ,NavLink} from 'react-router-dom';
 import Swal from 'sweetalert2';
+import { CDBSidebar, CDBSidebarContent, CDBSidebarFooter, CDBSidebarHeader, CDBSidebarMenu, CDBSidebarMenuItem } from 'cdbreact';
+import { logoutServiceCenter } from '../store/slices/ServiceCenter'; 
+import { fetchOrders } from '../store/slices/OrderRequestSlice';
 
 const SideBarCenter = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const orders = useSelector(state => state.orders.orders);
+  const ordersCount = orders.length; 
+
+  useEffect(() => {
+    dispatch(fetchOrders());
+  }, [dispatch]);
 
   const handleLogout = async () => {
     try {
@@ -27,6 +35,12 @@ const SideBarCenter = () => {
       Swal.fire('خطأ!', 'حدث خطأ أثناء تسجيل الخروج. يرجى المحاولة مرة أخرى.', 'error');
     }
   };
+
+  const [showCount, setShowCount] = useState(true);
+
+  const handleBellClick = () => {
+    setShowCount(false);
+  };
   return (
     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
       <CDBSidebar textColor="#fff" backgroundColor="#333">
@@ -39,11 +53,25 @@ const SideBarCenter = () => {
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
-            {/* <NavLink exact to="/maintenance-center/request-processing" activeClassName="activeClicked"> */}
-              {/* <CDBSidebarMenuItem icon="columns"> معالجة الطلبات </CDBSidebarMenuItem> */}
-            {/* </NavLink> */}
             <NavLink exact to="/maintenance-center/list-Notifications" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">الإشعارات <i class="bi bi-bell-fill"></i></CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="columns">
+  الإشعارات <i className="bi bi-bell-fill" onClick={handleBellClick}></i>
+  {ordersCount > 0 && showCount && (
+    <span className="notification-count" style={{
+      position: 'absolute',
+      top: '-10px',
+      right: '-10px',
+      backgroundColor: 'red',
+      color: 'white',
+      borderRadius: '50%',
+      padding: '0.25em 0.6em',
+      fontSize: '0.75rem',
+      fontWeight: 'bold'
+    }}>
+      {ordersCount}
+    </span>
+  )}
+</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/maintenance-center/list-maintenance-requests" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="columns"> قائمة الطلبات</CDBSidebarMenuItem>
@@ -53,6 +81,7 @@ const SideBarCenter = () => {
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
+
         <CDBSidebarFooter style={{ textAlign: 'center' }}>
           <div
             style={{
